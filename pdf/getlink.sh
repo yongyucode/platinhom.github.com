4f4b02ee63a768304ed31978ba820ca8a3d9aa41
platinhom <zhaozxcpu@hotmail.com>
2015-06-09 09:09:11 +0800

    regular

--- pdf/getlink.sh
+++ pdf/getlink.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-rm ./index.md
 echo "---">index.md
 echo "title: PDF">>index.md
 echo "layout: page">>index.md
@@ -8,7 +7,15 @@ echo "comments: yes">>index.md
 echo "---">>index.md
 echo "">>index.md
 
+echo "<short>" >> index.md
 for files in *.pdf */*.pdf
 do
 echo "[${files}](/pdf/${files})" >> index.md
 done
+echo "</short>" >> index.md
+
+if [ ! -z "`file index.md|grep ISO-8859`" ];then
+iconv -f GBK -t UTF-8 index.md > index-2.md
+rm index.md
+mv index-2.md index.md
+fi
