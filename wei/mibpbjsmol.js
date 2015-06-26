43b420cd4d7e1abafa7c502937dca40723f5d240
platinhom <zhaozxcpu@hotmail.com>
2015-06-27 02:28:05 +0800

    regular

--- wei/mibpbjsmol.js
+++ wei/mibpbjsmol.js
@@ -26,7 +26,7 @@ function initJmol(inmol,winsize){
       Jmol.script(jmolApplet0,'set platformSpeed 5');
       Jmol.script(jmolApplet0,'font echo 20 serif;fsize=20;set echo top center;echo MBIPB');
       Jmol.script(jmolApplet0,'set echo bottom left;font echo 16 bolditalic;color echo green;echo Powered by Jmol-14.2');
-      Jmol.jmolCommandInput(jmolApplet0, "cmd");
+      Jmol.jmolCommandInput(jmolApplet0, "Enter");
 }
 
 function showMolAs(){
@@ -109,6 +109,58 @@ function LoadResultSurface(){
 	}
 }
 
+
+// scroller.js gives simple div-based scroll
+// Bob Hanson 9:22 PM 7/21/2005
+// VERY simple div-based scroller.
+// body needs onload=initScrollers()
+//   then, somewhere: document.write(newScroller())
+//   with optional parameters: newScroller(name,caption,fCallback,width,x,y,isvertical,minvalue,maxvalue,initialvalue,factor)
+// fCallback must be string form of function, using _n, _v, _p (name, value, position)
+// For example, "newChart(Chart,'_n','_v','_p')
+// to set a scroller value from code, use resetScroller()
+
+Scrollers={}
+isScrollerInitialized=false
+thisMin = "-5";
+thisMax = "5";
+function doScroll(name, value) {
+	// callback from the scroller
+	alert("value:"+value)
+	var v = parseInt(value)
+	alert("v:"+v)
+	if (name == "min") {
+		if (v == thisMin)return
+		thisMin = v
+	} else {
+		if (v == thisMax)return
+		thisMax = v
+	}
+	alert("thisMin:"+thisMin)
+	document.getElementById("epsmin").value = thisMin
+	document.getElementById("epsmax").value = thisMax
+	//doRemap()
+}
+
+function checkScroll(name){
+	var pos=0
+	var value=0
+	var d=0
+	var S=Scrollers[name]
+	d=document.getElementById("scr_"+name)
+	pos=(S.isvertical?d.scrollTop:d.scrollLeft)
+	if(pos!=S.pos){
+		S.pos=pos //0 to 20*width
+		S.value=value=scrollValue(name)
+		setScrollCaption(name)
+		if(S.isinitialized)setTimeout(strScrollValues(S,S.callback),10)
+	}
+}
+
+function initScrollers(){
+	setTimeout('resetScroller("",-1)',100)
+}
+
 function newScroller(name,caption,fCallback,width,x,y,isvertical,minvalue,maxvalue,initialvalue,factor,fmouseup){
 	if(!name)name="scroll-test"
 	//if(!caption)caption="testing: pos=_p value=_v"
@@ -121,6 +173,7 @@ function newScroller(name,caption,fCallback,width,x,y,isvertical,minvalue,maxval
 	if(!maxvalue)maxvalue=100
 	if(isNaN(initialvalue))initialvalue=(maxvalue + minvalue)/2
 	if(!factor)factor=4
+
 	var s=""
 	var sout=""
 	var S=Scrollers[name]={}
@@ -138,9 +191,69 @@ function newScroller(name,caption,fCallback,width,x,y,isvertical,minvalue,maxval
 	S.initialvalue=initialvalue
 	// slimmed down version for the APBS application
 	var pos = (x < 0 ? "" : "position:absolute:left:" +  (isvertical ? (x-20) + ";top:" + y : x+";top:"+(y-40)))
-	sout="\n<div id='scr_"+name+"'  onscroll=\"checkScroll('"+name+"')\" style='font-size:2pt;height:25;width:"+width+";overflow:auto'>"
-		+"<img src='transp.gjf' height=1 width="+(width*(factor+1))+">"
+	sout="\n<div id='scr_"+name+"'  onscroll=\"checkScroll('"+name+"')\" style='font-size:2pt;height:25px;width:"+width+"px;overflow:auto'>"
+		+"<img src='transp.gif' height=1 width="+(width*(factor+1))+">"
 		+"</div>"
 	S.div=sout
 	return sout
+}
+
+
+
+function resetScroller(which,value){
+	for(var name in Scrollers){if(!which||name==which){
+			Scrollers[name].isinitialized=false
+			setScrollValue(name,0)
+			setScrollValue(name,(value>=0?value:Scrollers[name].initialvalue),1)
+			Scrollers[name].isinitialized=true
+	}}
+}
+
+function scrollValue(name){
+	var S=Scrollers[name]
+	var value=S.pos/(S.width*S.factor)*(S.maxvalue - S.minvalue) + S.minvalue
+	if(S.magnitude>1)value=Math.floor(value)
+	if(S.magnitude==1)value=Math.floor(value*100)/100
+	return value
+}
+
+function setScrollCaption(name){
+	var caption=""
+	var S=Scrollers[name]
+	if(S.caption.indexOf("_")>=0){
+		caption=strScrollValues(S,S.caption)
+		//document.title=name+" "+caption
+		document.getElementById("scr_"+name+"_caption").innerHTML=caption
+	}
+}
+
+function setScrollPosition(name,pos){
+	var d=0
+	var S=Scrollers[name]
+	d=document.getElementById("scr_"+name)
+	if(S.isvertical){
+		d.scrollTop=pos
+	}else{
+		d.scrollLeft=pos
+	}
+	S.pos=pos
+	S.value=scrollValue(name)
+	setScrollCaption(name)
+}
+
+function setScrollValue(name,value,dotrigger){
+	var S=Scrollers[name]
+	var pos=Math.floor((value - S.minvalue)/(S.maxvalue - S.minvalue)*S.width*S.factor)
+	S.pos=pos
+	S.value=scrollValue(name)
+	setScrollPosition(name,pos)
+	//	if(dotrigger)checkScroll(name)
+}
+
+function strScrollValues(S,what){
+	return (what + "('_n',_v,_p)").replace(/\_n/,S.name).replace(/\_p/,S.pos).replace(/\_v/,S.value)
+}
+
+function testScroll(name,value,position){
+	document.title="this is function testScroll('"+name+"',"+value+","+position+")"
 }
