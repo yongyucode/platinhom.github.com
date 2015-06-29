5e69a5419c9b1b5aa7cdc5c59f239d083ae8f574
platinhom <zhaozxcpu@hotmail.com>
2015-06-29 15:50:58 +0800

    regular

--- server/test.php
+++ server/test.php
@@ -13,10 +13,24 @@ function hi(){
 	echo $y;
 }
 $arr=array(1,2,3,4);
+$arr2=array(2=>0,5=>5);
+$arr3=$arr+$arr2;
+var_dump($arr3);
+
+$str="abcd";
 echo $x.";<br/>";echo $y;
 hi();
 echo "<br> {$x}","; ",$y,'<br>';
-echo $arr[1];
+echo $arr[1],"<br>";
+
+class People{}
+$p = new People();
+$t=true;
+var_dump($t);
+var_dump($str);
+var_dump($arr);
+var_dump($p);
+
 
 ?>
 </body>
