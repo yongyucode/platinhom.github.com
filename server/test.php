66af26a271a49cc567f424e5633b8f4b505ba612
platinhom <zhaozxcpu@hotmail.com>
2015-06-29 22:56:10 +0800

    regular

--- server/test.php
+++ server/test.php
@@ -2,36 +2,60 @@
 <html>
 <body>
 
+
 <?php
+//预处理提交信息
+// 定义变量并设置为空值
+$name = $email = $gender = $comment = $website = "";
+//处理必须输入的项.
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
 
-$x=5;
-$y=10;
-function hi(){
-	global $x;
-	echo $x;
-	global $y;
-	echo $y;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+ }
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
-$arr=array(1,2,3,4);
-$arr2=array(2=>0,5=>5);
-$arr3=$arr+$arr2;
-var_dump($arr3);
-
-$str="abcd";
-echo $x.";<br/>";echo $y;
-hi();
-echo "<br> {$x}","; ",$y,'<br>';
-echo $arr[1],"<br>";
-
-class People{}
-$p = new People();
-$t=true;
-var_dump($t);
-var_dump($str);
-var_dump($arr);
-var_dump($p);
-
-
 ?>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+Name: <input type="text" name="name"><span class="error">* <?php echo $nameErr;?></span><br><br>
+E-mail: <input type="text" name="email"><span class="error">* <?php echo $emailErr;?></span><br><br>
+Website: <input type="text" name="website"><span class="error"><?php echo $websiteErr;?></span><br><br>
+Comment: <textarea name="comment" rows="5" cols="40"></textarea><br><br>
+Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<span class="error">* <?php echo $genderErr;?></span><br><br>
+<input type="submit" name="submit" value="Submit"> 
+</form>
+
 </body>
 </html>
