b1e973ed093076a57623335bec5d664ad50cc5c5
platinhom <zhaozxcpu@hotmail.com>
2015-06-30 04:15:02 +0800

    regular

--- server/cgibin/prac.php
+++ server/cgibin/prac.php
@@ -45,6 +45,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   	echo "Probe radius: ".$probe."; Grid size is: ".$gride."; Buffer size is: ".$buffersize,"<br/>";
 	echo $_FILES["files"]["name"],"<br/>";
 
+  // $_FILES["file"]["error"] - 由文件上传导致的错误代码
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else    {//$_FILES["file"]["name"] - 被上传文件的名称
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    //$_FILES["file"]["type"] - 被上传文件的类型
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    //$_FILES["file"]["size"] - 被上传文件的大小，以字节计
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    //$_FILES["file"]["tmp_name"] - 存储在服务器的文件的临时副本的名称
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+	}
+
  }
 
 function test_input($data) {
