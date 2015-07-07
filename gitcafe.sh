0e23b4f984fc493e2110f112eb1e50af8c4df52a
platinhom <zhaozxcpu@hotmail.com>
2015-07-08 05:09:13 +0800

    regular

---
+++ gitcafe.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# file: submit.sh
+
+git add -A
+git commit -am "regular"
+#git push origin master
+#git remote add gitcafe git@gitcafe.com:platinhom/platinhom.git 
+git push gitcafe master:gitcafe-pages
