cb8c9675801cc37358262ba462cfd04b7c4c1f47
platinhom <zhaozxcpu@hotmail.com>
2015-11-30 02:47:13 -0500

    update-2015-11-30

--- gitsubmit.sh
+++ gitsubmit.sh
@@ -10,7 +10,7 @@ git add -A
 git commit -am "$comment"
 if [ ! -z $2 ];then
 	#git remote add gitcafe git@gitcafe.com:platinhom/platinhom.git 
-	git push gitcafe master:gitcafe-pages
+	git push gitcafe HEAD:gitcafe-pages
 fi
 # may be changed to your branch here
 git push origin master
